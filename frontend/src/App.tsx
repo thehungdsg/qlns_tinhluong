e2d90271b9ef92ed41f41758f38bd2a06fc73e8c
thehungdsg <thehung.itdsg@gmail.com>
2026-06-25 17:10:28 +0700

    feat:Create f frontend\src

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className=" flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-orange-500">Dự án Khóa luận Tốt Nghiệp 😂</h1>
+    </div>
+  )
+}
+
+export default App
